c928916ef523602fc077ef382029e4d56e87fdaf
Héctor Ramón Jiménez <hector0193@gmail.com>
2015-03-13 19:16:57 +0100

    Using preprocessor to avoid code duplication

--- src/super_ast/visitor.hpp
+++ src/super_ast/visitor.hpp
@@ -34,35 +34,35 @@ public:
 
   bool in_pre_order() const;
 
-  virtual void Visit(const Node* node, int depth);
-  virtual void Visit(const Block* block, int depth);
-  virtual void Visit(const Statement* statement, int depth);
-  virtual void Visit(const Type* type, int depth);
+  #define VISIT(TYPE) \
+  virtual void Visit(const TYPE* node, int depth);
 
-  // Statements
-  virtual void Visit(const Conditional* conditional, int depth);
-  virtual void Visit(const Declaration* declaration, int depth);
-  virtual void Visit(const Expression* expression, int depth);
-  virtual void Visit(const Loop* loop, int depth);
+  VISIT(Node)
+  VISIT(Block)
+  VISIT(Statement)
+  VISIT(Type)
 
-  // Declarations
-  virtual void Visit(const FunctionDeclaration* function, int depth);
-  virtual void Visit(const VariableDeclaration* variable, int depth);
+  VISIT(Conditional)
+  VISIT(Declaration)
+  VISIT(Expression)
+  VISIT(Loop)
 
-  // Expressions
-  virtual void Visit(const Atom* atom, int depth);
-  virtual void Visit(const BinaryOperator* binary_operator, int depth);
-  virtual void Visit(const UnaryOperator* unary_operator, int depth);
+  VISIT(FunctionDeclaration)
+  VISIT(VariableDeclaration)
 
-  // Loops
-  virtual void Visit(const For* for_loop, int depth);
-  virtual void Visit(const While* while_loop, int depth);
+  VISIT(Atom)
+  VISIT(BinaryOperator)
+  VISIT(UnaryOperator)
 
-  // Atoms
-  virtual void Visit(const FunctionCall* function_call, int depth);
-  virtual void Visit(const Identifier* identifier, int depth);
-  virtual void Visit(const Integer* integer, int depth);
-  virtual void Visit(const String* string, int depth);
+  VISIT(For)
+  VISIT(While)
+
+  VISIT(FunctionCall)
+  VISIT(Identifier)
+  VISIT(Integer)
+  VISIT(String)
+
+  #undef VISIT
 
 private:
   bool in_pre_order_;
